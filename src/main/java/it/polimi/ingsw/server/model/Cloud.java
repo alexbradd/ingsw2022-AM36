b21504202f05e9889884f7b35f2e36635867945a
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2022-04-26 23:04:07 +0200

    server/model/Cloud: implement StudentContainerInterface

--- src/main/java/it/polimi/ingsw/server/model/Cloud.java
+++ src/main/java/it/polimi/ingsw/server/model/Cloud.java
@@ -1,18 +1,23 @@
 package it.polimi.ingsw.server.model;
 
+import it.polimi.ingsw.server.model.enums.PieceColor;
+import it.polimi.ingsw.server.model.exceptions.ColorIsFullException;
+import it.polimi.ingsw.server.model.exceptions.ContainerIsFullException;
+import it.polimi.ingsw.server.model.exceptions.EmptyContainerException;
+
 import java.util.*;
 
 /**
  * This class models the game's clouds.
  *
- * @author Mattia Busso
+ * @author Mattia Busso, Alexandru Gabriel Bradatan
  */
-class Cloud {
+class Cloud implements StudentContainerInterface {
 
     /**
-     * Set of students on the cloud.
+     * This cloud's container
      */
-    private Set<Student> students;
+    private BoundedStudentContainer students;
 
     /**
      * The maximum number of students that can be present on the cloud.
@@ -31,7 +36,7 @@ class Cloud {
             throw new IllegalArgumentException("maxSize can't be <= 0");
         }
         this.maxSize = maxSize;
-        students = new HashSet<>();
+        students = new BoundedStudentContainer(maxSize);
     }
 
     /**
@@ -40,8 +45,8 @@ class Cloud {
      * @param old the Cloud to copy
      */
     Cloud(Cloud old) {
-        this.students = old.students;
         this.maxSize = old.maxSize;
+        this.students = old.students;
     }
 
     /**
@@ -50,7 +55,28 @@ class Cloud {
      * @return a tuple containing the Update cloud and the set of Students
      */
     Tuple<Cloud, Set<Student>> drainCloud() {
-        return new Tuple<>(new Cloud(this.maxSize), new HashSet<>(this.students));
+        return new Tuple<>(new Cloud(this.maxSize), this.students.getStudents());
+    }
+
+    /**
+     * Returns the number of students stored in this cloud
+     *
+     * @return the number of students stored in this cloud
+     */
+    @Override
+    public int size() {
+        return this.students.size();
+    }
+
+    /**
+     * It returns the number of Students of a certain color inside the container.
+     *
+     * @param color the color of the Students
+     * @return the number of Students of that color inside the container
+     */
+    @Override
+    public int size(PieceColor color) {
+        return students.size(color);
     }
 
     /**
@@ -66,7 +92,8 @@ class Cloud {
         }
         if (students.size() <= maxSize) {
             Cloud c = new Cloud(this);
-            c.students = new HashSet<>(students);
+            for (Student s : students)
+                c = c.add(s);
             return c;
         } else {
             throw new IllegalArgumentException("the new students size is larger than the allowed maxSize");
@@ -78,8 +105,44 @@ class Cloud {
      *
      * @return students_copy
      */
-    Set<Student> getStudents() {
-        return new HashSet<>(students);
+    @Override
+    public Set<Student> getStudents() {
+        return students.getStudents();
     }
 
+    /**
+     * This method removes a random {@link Student} from the container, and returns it with a new container instance, not
+     * containing the removed student.
+     *
+     * @return A {@link Tuple}<Container, Student> with the new container instance and the removed student
+     * @throws EmptyContainerException if the container is empty
+     */
+    @Override
+    public Cloud add(Student s) throws IllegalArgumentException, ContainerIsFullException, ColorIsFullException {
+        Cloud c = new Cloud(this);
+        c.students = c.students.add(s);
+        return c;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Tuple<Cloud, Student> remove() throws EmptyContainerException {
+        Cloud c = new Cloud(this);
+        Tuple<BoundedStudentContainer, Student> t = c.students.remove();
+        c.students = t.getFirst();
+        return t.map((container, s) -> new Tuple<>(c, t.getSecond()));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Tuple<Cloud, Student> remove(PieceColor color) throws IllegalArgumentException, EmptyStackException {
+        Cloud c = new Cloud(this);
+        Tuple<BoundedStudentContainer, Student> t = c.students.remove(color);
+        c.students = t.getFirst();
+        return t.map((container, s) -> new Tuple<>(c, t.getSecond()));
+    }
 }
